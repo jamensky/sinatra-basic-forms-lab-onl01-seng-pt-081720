edd39f690f35d3ed7c848766c56b847c37d6621d
Jamal Rjoul <jamensky1991@gmail.com>
2020-11-11 19:35:54 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,9 +2,9 @@ class Puppy
   attr_reader :name, :breed, :age
 
   def initialize(name, breed, age)
-    @name = name 
-    @breed = breed 
-    @age = age 
-  end 
+    @name = name
+    @breed = breed
+    @age = age
+  end
 
 end
