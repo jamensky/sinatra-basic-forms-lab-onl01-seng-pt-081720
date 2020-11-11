05eb55614deea5243a00b2a141d83dccf88153e6
Jamal Rjoul <jamensky1991@gmail.com>
2020-11-11 20:10:08 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,10 +1,10 @@
 class Puppy
   attr_accessor :name, :breed, :age
 
-  def initialize(name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
+  def initialize(args)
+    @name = args[:name]
+    @breed = args[:breed]
+    @age = args[:age]
   end
 
 end
